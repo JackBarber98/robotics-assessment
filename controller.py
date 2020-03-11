09cbf2812879c9e2c25b6d1e68245893bf7c4316
Jack <34523565+JackBarber98@users.noreply.github.com>
2020-03-11 18:41:18 +0000

    It works now!

--- controller.py
+++ controller.py
@@ -5,90 +5,63 @@ from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import String
 
-from cv2 import namedWindow, cvtColor, imshow
-from cv2 import destroyAllWindows, startWindowThread
-from cv2 import COLOR_BGR2GRAY, waitKey
-from cv2 import blur, Canny
-from numpy import mean
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
-
 class Controller:
     def __init__(self):
         rospy.init_node("controller")
-        self.corner_examinder_publisher = rospy.Publisher("/corner_examiner", String, queue_size=1)
 
-        self.laser_subscriber = rospy.Subscriber("/scan", LaserScan, self.laser_callback)
-        self.odom_subscriber = rospy.Subscriber("/odom", Odometry, self.odom_callback)
-        self.twist_publisher = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=1)
+        self.__laser_subscriber = rospy.Subscriber("/scan", LaserScan, self.laser_callback)
+        self.__twist_publisher = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=1)
+        
+        self.__twist = Twist()
 
-        self.left_third = []
-        self.middle_third = []
-        self.right_third = []
+        self.left_laser_sum = 0
+        self.right_laser_sum = 0
+        self.laser_minimum = 0
+        self.laser_maximum = 0
         self.laser_data = []
 
-        self.odom = []
-
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.image_callback)
-
     def laser_callback(self, laser_data):
+        laser_data = laser_data.ranges
+        
+        self.left_laser_sum = 0 
+        self.right_laser_sum = 0
+        self.laser_minimum = np.nanmin(laser_data)
+        self.laser_maximum = np.nanmax(laser_data)
+
+        self.left_laser_sum = np.nansum(laser_data[:len(laser_data) / 2])
+        self.right_laser_sum = np.nansum(laser_data[len(laser_data) / 2:])
+
         self.laser_data = laser_data
-        self.left_third = np.array(laser_data.ranges)[  : len(laser_data.ranges) / 3]
-        self.middle_third = np.array(laser_data.ranges)[len(laser_data.ranges) / 3 : 2 * len(laser_data.ranges) / 3]
-        self.right_third = np.array(laser_data.ranges)[2 * len(laser_data.ranges) / 3 : ]
-
-    def odom_callback(self, odom_data):
-        self.odom = odom_data.pose.pose.orientation
-
-    def image_callback(self, image_data):
-        namedWindow("Image window")
-
-        cv_image = self.bridge.imgmsg_to_cv2(image_data, "bgr8")
-
-        gray_img = cvtColor(cv_image, COLOR_BGR2GRAY)
-        img3 = Canny(gray_img, 0, 0)
-        imshow("canny", img3)
-        imshow("normal", cv_image)
-        waitKey(1)
-
-    def is_close_to_wall(self):
-        if np.less(self.middle_third, 0.6).any() or np.less(self.left_third, 0.5).any():
-            return True
-
-    def get_forward_distance(self):
-        return np.less(self.middle_third, 0.5).any()
-
-    def get_distance_right(self):
-        return np.nanmean(self.right_third)
-
-    def get_distance_left(self):
-        return np.nanmean(self.left_third)
-
-    def forward(self):
-        twist = Twist()
-        twist.linear.x = 0.2
-        self.twist_publisher.publish(twist)
-
-    def get_shortest_distance(self):
-        return self.laser_data
-
-    def turn_round(self):
-        twist = Twist()
-        twist.angular.z = 5
-        self.twist_publisher.publish(twist)
-
-    def left(self):
-        twist = Twist()
-        twist.angular.z = -1.1
-        self.twist_publisher.publish(twist)
-        rospy.sleep(0.1)
     
-    def right(self):
-        twist = Twist()
-        twist.linear.x = 0.1
-        twist.angular.z = 1
-        self.twist_publisher.publish(twist)
+    def forwards(self):
+        self.__twist.angular.z = 0
+        self.__twist.linear.x = 0.2
+        self.__twist_publisher.publish(self.__twist)
+
+    def backwards(self):
+        self.__twist.angular.z = 0
+        self.__twist.linear.x = -0.1
+        self.__twist_publisher.publish(self.__twist)
+
+    def stop(self):
+        self.__twist.angular.z = 0
+        self.__twist.linear.x = 0
+        self.__twist_publisher.publish(self.__twist)
+
+    def turn_left(self):
+        self.__twist.angular.z = -0.5
+        self.__twist.linear.x = 0
+        self.__twist_publisher.publish(self.__twist)
+
+    def turn_right(self):
+        self.__twist.angular.z = -0.5
+        self.__twist.linear.x = 0
+        self.__twist_publisher.publish(self.__twist)
+
+    def drift_right(self):
+        self.__twist.linear.x = 0.2
+        self.__twist.angular.z = 0.1
+        self.__twist_publisher.publish(self.__twist)
 
     def run(self):
         rospy.spin()
--- main.py
+++ main.py
@@ -10,12 +10,41 @@ from controller import Controller
 def solve():
     controller = Controller()
 
-    turn_left = False
+    following_wall = False
+    door_detected = False
 
     while not rospy.is_shutdown():
-        if not controller.is_close_to_wall():
-            controller.forward()
-        else:
-            controller.left()
+        #try:
+        if controller.laser_minimum < 0.5 and not door_detected:
+            print("too close...") 
+            if not door_detected:
+                print("and the door isn't detected")
+                following_wall = True
+                controller.stop()
+                if controller.left_laser_sum >= controller.right_laser_sum:
+                    controller.turn_right()
+                else:
+                    controller.turn_left()
+            else:
+                print("and the door is detected")
+        else: 
+            if following_wall:
+                if controller.laser_maximum >= 2:
+                    door_detected = True
+                    following_wall = False
+                    print("door detected")
+            if door_detected:
+                print("laser min: ", controller.laser_minimum)
+                if controller.laser_minimum < 0.5:
+                    print("gonna crash into wall (drifting)")
+                    door_detected = False
+                else:
+                    print("drifting...")
+                    controller.drift_right()
+            elif controller.laser_minimum >= 0.5:
+                controller.forwards()
+        # except:
+        #     controller.backwards()
+        rospy.sleep(0.5)
 
 solve()
