9338b70c11d54ded99ec6b24051dd1ad62ff5d1d
Jack <34523565+JackBarber98@users.noreply.github.com>
2020-03-27 13:38:20 +0000

    Starts adding docstrings

--- controller.py
+++ controller.py
@@ -1,24 +1,28 @@
 import rospy
 import numpy as np 
-from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
+
 from sensor_msgs.msg import LaserScan, Image
+from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 
-from cv2 import namedWindow, cvtColor, imshow, inRange
-from cv2 import destroyAllWindows, startWindowThread
-from cv2 import COLOR_BGR2GRAY, waitKey
-from cv2 import Canny
+from cv2 import imshow, inRange, destroyAllWindows, waitKey
 from cv_bridge import CvBridge
 
 class Controller:
     def __init__(self):
         rospy.init_node("controller")
 
-        self.__laser_subscriber = rospy.Subscriber("/scan", LaserScan, self.laser_callback)
+        # Subscribers are required to read data from the robot's sensors. Publishers are required to 
+        # control the robot. Double underscores are used to denote private variables that cannot 
+        # be accessed by other classes.
+        self.__laser_subscriber = rospy.Subscriber("/scan", LaserScan, self.__laser_callback)
+        self.__camera_subscriber = rospy.Subscriber("camera/rgb/image_raw", Image, self.__camera_callback)
         self.__twist_publisher = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=1)
-        
+
         self.__twist = Twist()
+
+        # Provides a connection to the OpenCV library.
+        self.__cv_bridge = CvBridge()
 
         self.left_laser_sum = 0
         self.right_laser_sum = 0
@@ -33,70 +37,136 @@ class Controller:
         self.blue_mask_left = []
         self.blue_mask_right = []
 
-        self.__cv_bridge = CvBridge()
-        self.__camera_subscriber = rospy.Subscriber("camera/rgb/image_raw", Image, self.camera_callback)
-
-    def laser_callback(self, laser_data):
+    def __laser_callback(self, laser_data):
+        """ Processes laser data recieved from the robot to calculate the minimum and maximum distances, 
+        as well as the sum of the laser values of the left and right halves of the laser array. 
+        
+        Parameters:
+        laser_data (LaserScan): The raw data recieved from the robot's Kinect infrared laser sensor.
+        
+        """
+        print(type(laser_data))
         laser_data = laser_data.ranges
 
         try:
             np.nanargmax(laser_data)
         except:
             laser_data = np.zeros(np.shape(laser_data))
+
+        self.laser_data = laser_data
         
-        self.left_laser_sum = 0 
-        self.right_laser_sum = 0
         self.laser_minimum = np.nanmin(laser_data)
         self.laser_maximum = np.nanmax(laser_data)
 
         self.left_laser_sum = np.nansum(laser_data[:len(laser_data) / 2])
         self.right_laser_sum = np.nansum(laser_data[len(laser_data) / 2:])
 
-        self.laser_data = laser_data
+    def __camera_callback(self, camera_data):
 
-    def camera_callback(self, camera_data):
+        """ Receives raw camera data from the robot and uses it to determine whether a green, 
+        red, or blue square can be seen. OpenCV is used to aid this. 
+        
+        Parameters:
+        camera_data (Image): Raw data recieved via the Image ROS topic.
+        
+        """
+
         cv_image = self.__cv_bridge.imgmsg_to_cv2(camera_data, "bgr8")
 
-        green_mask = inRange(cv_image[:, [100, 400]], (1, 130, 1), (1, 160, 1))
-        red_mask = inRange(cv_image[:][400:420], (1, 1, 130), (1, 1, 160))
-        blue_mask = inRange(cv_image, (120, 20, 20), (130, 30, 30))
+        self.__set_green_square_flag(cv_image)
+        self.__set_red_square_flag(cv_image)
+        self.__set_blue_square_flag(cv_image)
 
-        self.blue_mask_left = blue_mask[:, :320]
-        self.blue_mask_right = blue_mask[:, 320:]
+        waitKey(1)
+
+    def __set_green_square_flag(self, cv_image):
+
+        """ Changes the value of the green_square_found flag depending on whether 
+        any green pixels can be found in the robot's field of view. 
+        
+        Parameters:
+        cv_image (numpy.ndarray): A 3D array containing BGR pixel data from the robot's camera. 
+        
+        """
+
+        print(type(cv_image))
+
+        green_mask = inRange(cv_image[:, [100, 400]], (1, 130, 1), (1, 160, 1))
 
         if np.in1d(green_mask, 255, assume_unique=True).any():
             self.green_square_found = True
         else:
             self.green_square_found = False
+    
+    def __set_red_square_flag(self, cv_image):
+
+        """ Changes the value of the red_square_found flag depending on whether 
+        any red pixels can be found in the robot's field of view. 
+        
+        Parameters:
+        cv_image (numpy.ndarray): A 3D array containing BGR pixel data from the robot's camera. 
+        
+        """
+
+        red_mask = inRange(cv_image[:][400:420], (1, 1, 130), (1, 1, 160))
 
         if np.in1d(red_mask, 255, assume_unique=True).any():
             self.red_square_found = True
         else:
             self.red_square_found = False
 
+    def __set_blue_square_flag(self, cv_image):
+
+        """ Changes the value of the blue_square_found flag depending on whether 
+        any blue pixels can be found in the robot's field of view. The left and right halves of
+        the mask are stored as seperate public variables.
+        
+        Parameters:
+        cv_image (numpy.ndarray): A 3D array containing BGR pixel data from the robot's camera. 
+        
+        """
+
+        blue_mask = inRange(cv_image, (120, 20, 20), (130, 30, 30))
+
         if np.in1d(blue_mask, 255, assume_unique=True).any():
             self.blue_square_found = True
         else:
             self.blue_square_found = False
 
-        waitKey(1)
+        self.blue_mask_left = blue_mask[:, :320]
+        self.blue_mask_right = blue_mask[:, 320:]
     
     def forwards(self):
+
+        """ Publishes twist data that will be used to make the robot move forward. A slight
+        leftward drift is applied to encourage the robot to follow the left-hand wall of the maze. """
+
         self.__twist.angular.z = 0.05
         self.__twist.linear.x = 0.2
         self.__twist_publisher.publish(self.__twist)
 
     def backwards(self):
+
+        """ Publishes twist data that will be used to make the robot move backwards. The robot recieves 
+        this data on the ./velocity topic. """
+
         self.__twist.angular.z = 0
         self.__twist.linear.x = -0.1
         self.__twist_publisher.publish(self.__twist)
 
     def stop(self):
+
+        """ Publishes twist data that will be used to make the robot stop all movement. """
+
         self.__twist.angular.z = 0
         self.__twist.linear.x = 0
         self.__twist_publisher.publish(self.__twist)
 
     def turn_left(self):
+
+        """ Publishes twist data that will be used to make the robot turn left with an 
+        angular velocity of 0.5 radians per second. """
+
         self.__twist.angular.z = 0.5
         self.__twist.linear.x = 0
         self.__twist_publisher.publish(self.__twist)
